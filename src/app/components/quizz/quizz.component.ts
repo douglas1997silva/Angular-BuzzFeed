fc023e16c8640ba53f218d23d4d9369811855ed2
Douglas Oliveira <douglas.olliveira1997@gmail.com>
2024-01-16 17:42:57 -0300

    Buzz Feed

---
+++ src/app/components/quizz/quizz.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quizz',
+  standalone: true,
+  imports: [],
+  templateUrl: './quizz.component.html',
+  styleUrl: './quizz.component.css'
+})
+export class QuizzComponent {
+
+}
