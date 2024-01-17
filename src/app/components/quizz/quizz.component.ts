bab7c9424dcff4fa611d73335094c64426bd07c6
Douglas Oliveira <douglas.olliveira1997@gmail.com>
2024-01-17 16:34:55 -0300

    Logica

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import quizz_questions from '../../../assets/data/quizz_questions.json'
+
 
 @Component({
   selector: 'app-quizz',
@@ -8,5 +10,42 @@ import { Component } from '@angular/core';
   styleUrl: './quizz.component.css'
 })
 export class QuizzComponent {
+ titles: string = '';
+ questions:any
+ questionSelection:any
 
+ answers: string[]= [];
+ answersSelect:string =""
+
+ questionIndex:number = 0;
+ questionMaxIndex:number = 0;
+
+ finished:boolean = false;
+
+ ngOnInit():void{
+   if(quizz_questions){
+    this.finished = false;
+    this.titles = quizz_questions.title;
+    this.questions = quizz_questions.questions;
+    this.questionSelection = this.questions[this.questionIndex]
+
+    this.questionIndex = 0;
+    this.questionMaxIndex = this.questions.length;
+   }
+ }
+
+ playChoose(value:string){
+  this.answers.push(value);
+
+ }
+
+ async nextStep (){
+
+  this.questionIndex += 1;
+  if(this.questionMaxIndex > this.questionIndex){
+   this.questionSelection = this.questions[this.questionIndex]
+  }else{
+  this.finished = true ;
+  }
+ }
 }
